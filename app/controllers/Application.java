ffe8ffd0b08242779c33670c2603ce69c85098b5
Aicha34 <at-aicha@hotmail.com>
2012-11-26 14:02:51 +0100

    liste des messages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,26 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
+import models.Message;
+import models.UserAccount;
+import play.*;
+import play.data.Form;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static Form<Message> messageForm = form(Message.class);
+	static Form<UserAccount> userForm = form(UserAccount.class);
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+public static Result index() {
+	  return redirect(routes.Application.index());
+}
   
+public static Result messages() {
+	
+			return ok(views.html.messages.render(Message.findAll(), messageForm));	
+		}
+
 }
